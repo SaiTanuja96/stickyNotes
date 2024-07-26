64b17ea6c366f16f8d45f55a9391be1d5ce61852
Tanuja Batchali <34961894+SaiTanuja96@users.noreply.github.com>
2024-07-26 13:39:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-})
+// export default defineConfig({
+//   plugins: [preact()],
+// })
+export default {
+  build: {
+    rollupOptions: {
+      external: 'src/main.jsx'
+    }
+  }
+}
